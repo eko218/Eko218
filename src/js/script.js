66d03e47d008479be57d850fec7c2a481ca7afb5
eko <e_c.n_v@icloud.com>
2023-11-20 16:55:20 +0900

    ヘッダー更新

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,29 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-
+// ハンバーガーメニュー
+// $(function(){
+//     $('.header__drawer').on('click', function() {
+//       $(this).toggleClass('is-active');
+//       return false;
+//     });
+//   });
+  
+  $(function () {
+    // ハンバーガーメニュー
+   $(".js-hamburger,.js-drawer-menu").on('click', function () {
+     if ($(".js-hamburger").hasClass("is-active")) {
+       // $(".js-drawer-menu").fadeOut();
+       $("body, html").css("overflow", "auto");
+       $(".js-drawer-menu").removeClass("is-active");
+       $(this).removeClass("is-active");
+    } else {
+       //  $(".js-drawer-menu").fadeIn();
+       $("body, html").css("overflow", "hidden");
+       $(".js-drawer-menu").addClass("is-active");
+        $(this).addClass("is-active");
+    }
+    });
+  });
 });
+
